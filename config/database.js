3c0bda636b2a3d0574ea88cfbeaba8f1488c840d
Jéssica Ramírez <jessicaramsa@gmail.com>
2022-04-11 13:32:34 -0500

    Chore: Change database connection to allow connections to planetscale

--- config/database.js
+++ config/database.js
@@ -54,8 +54,11 @@ module.exports = {
       port: Env.get('DB_PORT', ''),
       user: Env.get('DB_USER', 'root'),
       password: Env.get('DB_PASSWORD', ''),
-      database: Env.get('DB_DATABASE', 'adonis')
-    }
+      database: Env.get('DB_DATABASE', 'adonis'),
+      ssl: {
+        rejectUnauthorized: true
+      }
+    },
   },
 
   /*
